83f9f4b20a5f0dda072284a67c7a4bf70feb014d
Elise Neven <elise.neven@uliege.be>
2024-09-04 10:06:11 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,4 @@
 # Configuration file for the Sphinx documentation builder.
-#
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
@@ -14,21 +13,53 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc'] # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html to generate documentation from docstrings
+extensions = [
+    'sphinx.ext.autodoc',   # To generate documentation from docstrings
+    'sphinx.ext.napoleon',  # For Google/NumPy style docstrings
+    'sphinx.ext.viewcode',  # Add links to highlighted source code
+    'sphinx.ext.todo',      # Support for TODO notes
+]
+
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'private-members': True,
+    'special-members': True,
+    'inherited-members': True,
+    'show-inheritance': True,
+}
+
+autodoc_member_order = 'bysource'
+autodoc_typehints = 'description'
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**/.ipynb_checkpoints']
 
 import os
 import sys
 # Update this path to the directory containing your library source code
 sys.path.insert(0, os.path.abspath('../library'))
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_title = "PyLaboThap Documentation"
 
+html_theme_options = {
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False,
+}
+
+# Uncomment and configure if you have a logo or favicon
+# html_logo = '_static/logo.png'
+# html_favicon = '_static/favicon.ico'
+
+# Optional custom CSS or JS
+# html_css_files = ['css/custom.css']
+# html_js_files = ['js/custom.js']
 
