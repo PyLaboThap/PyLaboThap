c1f656840ec375bcdd168ddd1f8d298e8bb60f14
Elise Neven <elise.neven@uliege.be>
2024-09-04 14:36:47 +0200

    doc string isentropic compressor model

---
+++ docs/build/_sources/constant_isentropic_efficiency_compressor.rst.txt
@@ -0,0 +1,9 @@
+MyModel
+========
+
+.. autoclass:: component.steady_state.volumetric_machine.compressor.constant_isentropic_efficiency.simulation_model.CompressorCstEff
+    :members:
+    :undoc-members:
+    :private-members:
+    :special-members:
+    :inherited-members:
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,18 +1,16 @@
-# Configuration file for the Sphinx documentation builder.
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+
+# Add the path to the library directory
+sys.path.insert(0, os.path.abspath('../../library'))
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'PyLaboThap'
 copyright = '2024, Basile Chaudoire, Elise Neven'
 author = 'Elise Neven, Basile Chaudoire'
 release = '1.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     'sphinx.ext.autodoc',   # To generate documentation from docstrings
     'sphinx.ext.napoleon',  # For Google/NumPy style docstrings
@@ -35,14 +33,7 @@ autodoc_typehints = 'description'
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**/.ipynb_checkpoints']
 
-import os
-import sys
-# Update this path to the directory containing your library source code
-sys.path.insert(0, os.path.abspath('../library'))
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_title = "PyLaboThap Documentation"
@@ -55,11 +46,4 @@ html_theme_options = {
     'titles_only': False,
 }
 
-# Uncomment and configure if you have a logo or favicon
-# html_logo = '_static/logo.png'
-# html_favicon = '_static/favicon.ico'
-
-# Optional custom CSS or JS
-# html_css_files = ['css/custom.css']
-# html_js_files = ['js/custom.js']
 
