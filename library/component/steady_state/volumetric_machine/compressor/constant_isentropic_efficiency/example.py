01630ea65d5a4ee2d63aa3c13137418be37db0f3
Elise Neven <155994101+el-neven@users.noreply.github.com>
2024-09-11 14:58:23 +0200

    Update example.py

--- library/component/steady_state/volumetric_machine/compressor/constant_isentropic_efficiency/example.py
+++ library/component/steady_state/volumetric_machine/compressor/constant_isentropic_efficiency/example.py
@@ -12,7 +12,7 @@ import numpy as np
 CP = CompressorCstEff()
 CP.print_setup()
 
-# "If the inputs are not set directly BUT throught the connectors"
+# "If the inputs are not set directly BUT through the connectors"
 CP.su.set_properties(P=319296.5575177148, T=331.033964665788, fluid='R1233ZDE')
 CP.ex.set_properties(P=606240.1433176235, fluid='R1233ZDE')
 CP.set_parameters(eta_is=0.8)
@@ -22,5 +22,3 @@ CP.solve()
 CP.print_results()
 CP.plot_component_comp_cst_eff()
 CP.plot_connectors_comp_cst_eff()
-
-"Hallo"
